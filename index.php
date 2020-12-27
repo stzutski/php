ce988ad188e4a81d2d3b97dc3192998263ec0785
Roberto Stzutski <roberto.stzutski@gmail.com>
2020-12-27 16:08:31 -0300

    reset do arquivo

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 // Script para apresentação da funcionalidade do github aula 001
 
-echo "Olá mundo 456";
+// echo "Olá mundo 456";
 
-echo "Ola desenvolvedor e programadores!!!";
+// echo "Ola desenvolvedor e programadores!!!";
 
 ?>
